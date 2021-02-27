8bd042d5f83377a2fff0bfe2e2fdbf268ec58246
Jessica Antunes <jessantunes@protonmail.com>
2021-02-27 10:41:42 -0500

    commit tree a63c25a4ffd22871af6f3cca1de3668531ad7f5b

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+  array.each.with_index(1) { |dwarf, number| puts "#{number}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)# code an argument here
+  
+  array.collect {|element| "#{element.capitalize}!" }
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  
+  return true if array.any? { |word| word.length > 4 }
+  return false
+  
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese| 
+    if array.include?(cheese) == true
+      return cheese
+    else
+      return nil
+    end
+  end    
 end
